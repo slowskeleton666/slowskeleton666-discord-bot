c7edc800fa54cac71beaf3584b60d690a0aea713
slowskeleton666 <143322350+slowskeleton666@users.noreply.github.com>
2023-09-06 10:50:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
